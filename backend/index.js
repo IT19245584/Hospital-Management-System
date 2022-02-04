117a4f2f75d3f42ee5177242db52167837337933
Nadun Sandeepa <68732378+IT19245584@users.noreply.github.com>
2022-02-04 22:32:23 +0530

    two

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+
+// Entry Point of the API Server 
+  
+const express = require('express');
+  
+/* Creates an Express application. 
+   The express() function is a top-level 
+   function exported by the express module.
+*/
+const app = express();
+const Pool = require('pg').Pool;
+  
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'gfgbackend',
+    password: 'postgres',
+    dialect: 'postgres',
+    port: 5432
+});
+  
